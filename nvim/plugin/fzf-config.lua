a18d0c1d5d9d64d19e94370920fa296d3d78b047
Dane Gilbert <daneg89@gmail.com>
2024-11-20 11:56:20 -0500

    vim-go setting disabled because clashed with harpoon ctrl-t binding. ctrlsf changed to not send results to quickfix list

--- nvim/plugin/fzf-config.lua
+++ nvim/plugin/fzf-config.lua
@@ -4,7 +4,8 @@ vim.keymap.set('v', "<S-f>", "<Plug>CtrlSFVwordPath")
 vim.g.ctrlsf_ackprg='rg'
 -- Just go to the window to browse search results
 vim.g.ctrlsf_auto_focus = { at = "start" }
--- Results go to quickfix so we can use cnext
-vim.g.ctrlsf_populate_qflist = 1
 -- Was seeing errors from this, just disable it
 vim.g.ctrlsf_fold_result = 0
+
+-- Results go to quickfix so we can use cnext
+-- vim.g.ctrlsf_populate_qflist = 1
--- nvim/plugin/vim-go.lua
+++ nvim/plugin/vim-go.lua
@@ -1,1 +1,4 @@
 vim.g.go_doc_keywordprg_enabled = 0
+
+-- Disables C-t binding defined by vim-go for godef stack jump which interferes with custom harpoon binding
+vim.g.go_def_mapping_enabled = 0
